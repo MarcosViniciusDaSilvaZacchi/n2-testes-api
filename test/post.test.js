b2d0013ccb37a42fd92c15f7cbf2272679429c38
Bruno Costa <bruno.costa.dev@outlook.com>
2025-10-24 13:43:15 -0300

    Instal dependencias e criacao logica test addPost

--- test/post.test.js
+++ test/post.test.js
@@ -1,7 +1,26 @@
 const sinon = require('sinon');
 const chai = require('chai');
-const usuario = require('../src/user.js');
-const post = require('../src/post.js');
+const postagem = require('../src/post.js');
 
 const expect = chai.expect;
 const assert = chai.assert;
+
+beforeEach(() => {
+    usuario.resetUsers();
+});
+
+describe("addPost", ()=>{
+   it("Deve criar e associar corretamente um novo post ao ID do usuário", ()=>{
+        const userId = 1
+        const newPost = {
+            date: "24/10/2025",
+            categoria: "Política",
+            texto: "Os politicos são corruptos, por isso a população passa fome."
+        };
+        postagem.addPost(1,newPost);
+        const postCriado = postagem.getPost(userId)[0];
+
+        expect(postCriado).to.be.exist;
+
+    });
+});
