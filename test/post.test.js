6579e3c715a7cb8e84ec565b864159179b8209a0
Bruno Costa <bruno.costa.dev@outlook.com>
2025-10-24 12:56:16 -0300

    Criacao da estrutura para teste de post

---
+++ test/post.test.js
@@ -0,0 +1,7 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const usuario = require('../src/user.js');
+const post = require('../src/post.js');
+
+const expect = chai.expect;
+const assert = chai.assert;
