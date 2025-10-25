6bb5ef0ba07c1f64ea17bc0160d4b1a69a8dc329
brunno.asquino <brunnof.asquino@gmail.com>
2025-10-25 03:34:22 -0300

    Funções e funções de teste para o arquivo post.js finalizado

--- src/post.js
+++ src/post.js
@@ -1,36 +1,72 @@
 const getUser = require('./user.js')
 
 function createPost(userId, post){
-    if(!post.text || post.text.trim().length === 0) throw new Error("O Post deve ter um conteúdo de texto válido.");
-    if(!post.date || post.date.trim().length === 0) throw new Error("Data não pode ser vazia");
-    if(!post.category || post.category.trim().length === 0) throw new Error("Categoria não pode ser vazia.");    
+    if (!post)
+        throw new Error("Dados inválidos");
+
+    if (!post.id || typeof post.id !== "number" || isNaN(post.id) || post.id <= 0)
+        throw new Error("ID inválido");
+
+    if (!post.content || post.content.trim().length === 0)
+        throw new Error("Conteúdo inválido");
+
+    if (!post.category || post.category.trim().length === 0)
+        throw new Error("Categoria inválida");
+
+    if (!post.createdAt)
+        throw new Error("Data inválida");
+
+    if(!(post.createdAt instanceof Date)) post.createdAt = new Date (post.createdAt);
+
     const user = getUser.getUserById(userId);
     if(!user) throw new Error("Usuário não encontrado");
-    user.posts.push(post);
+
+    if (user.posts.find(p => p.id === post.id)) throw new Error("ID já está cadastrado");
+    
+    user.posts.push({ ...post, comments:[] });
 }
 
 function removePost(userId, index){
     const user = getUser.getUserById(userId);
-    if(!user || index < 0 || index >= user.posts.length) throw new Error("Post não encontrado.");
+    if(!user || index < 0 || index >= user.posts.length) throw new Error("Usuário não encontrado.");
     user.posts.splice(index, 1);
 }
 
 function searchPosts(userId){
     const user = getUser.getUserById(userId);
     if(!user) return[];
+
     return[...user.posts];
 }
 
 function searchPostCategory(userId, category){
     const user = getUser.getUserById(userId);
     if(!user) return[];
+
     return user.posts.filter(post => post.category === category);
 }
 
 function searchPostID(userId, postId){
     const user = getUser.getUserById(userId);
     if(!user) return[];
-    return user.posts.filter(post => post.id === postId);
+
+    return user.posts.find(p => p.id === postId);
+}
+
+function getPostsByRangeDate(userId, dateStart, dateEnd) {
+    const user = getUser.getUserById(userId);
+    if(!user) return[];
+
+    dateStart = new Date(dateStart);
+    dateEnd = new Date(dateEnd);
+
+    if (dateStart > dateEnd) 
+        throw new Error('A data de inicio não deve ser menor que a data final');
+
+    return user.posts.filter(post => {
+        const postDate = new Date(post.createdAt);
+        return postDate >= dateStart && postDate <= dateEnd;
+    });
 }
 
 module.exports = {
@@ -38,5 +74,6 @@ module.exports = {
     removePost,
     searchPosts,
     searchPostCategory,
-    searchPostID
+    searchPostID,
+    getPostsByRangeDate
 }
